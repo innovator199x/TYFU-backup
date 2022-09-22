d36776e8801b6a107829efbf2879a251d06763b5
Dave Flores Malhin <111928156+innovator199x@users.noreply.github.com>
2022-09-22 13:00:25 +0800

    Update lang

--- connection.php
+++ connection.php
@@ -7,9 +7,9 @@ if ($_SERVER['SERVER_NAME'] == 'localhost') {
     $db_name = "tyfu_db";
 } else { //live db
     $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "tyfu_db";
+    $db_user = "dreeve_tyfu";
+    $db_pass = "!eHbk?*q^[2F";
+    $db_name = "dreeve_tyfu_db";
 }
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
